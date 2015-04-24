7cbc48cb3d08b81ded3c7cbbcee0276600ca3527
erickcallao <ericck-89@hotmail.com>
2015-04-24 10:42:40 -0500

    holi

---
+++ 2.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+
+	</head>
+		<bady>
+			<form method="get" action="22.php">	
+				Numero: 			
+				<input type="tex" name ="n">
+				<p>
+				<input type="submit" value ="Aceptar">
+			</form>
+		</bady>
+</html>
