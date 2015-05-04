cf8dd23a09013be518f7d232dafae8ac6f8afa47
carito06 <barbycorazon@hotmail.com>
2015-05-04 16:29:52 -0500

    aaaaa

---
+++ 9.php
@@ -0,0 +1,52 @@
+<html>
+	<head>
+
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"><style type="text/css">
+<!--
+body,td,th {
+	color: #FFFFFF;
+}
+body {
+	background-image: url(f.php_1.jpg);
+}
+-->
+</style></head>
+		<bady>
+			<form method="get" action="99.php">	
+				<center>
+				<br><br><br>
+				<br>Crear una aplicacion que permita el ingreso de la fecha de nacimiento de una 
+				<br>persona y en funcion a ello emitir un mensaje si es mayor de edad o menor de edad.
+				<p>
+				Fecha de nacimiento:
+				<p>
+				Fecha 			
+				<input type="tex" name ="fec">
+				<p>
+				Mes:
+				<select name="mes">
+				<option>Enero</option>
+				<option>Febrero</option>
+				<option>Marzo</option>
+				<option >Abril</option>
+				<option >Mayo</option>
+				<option >Junio</option>
+				<option >Julio</option>
+				<option >Agosto</option>
+				<option >Septiembre</option>
+				<option >Octubre</option>
+				<option >Nonviembre</option>
+				<option >Diciembre</option>
+				</select>
+				<p>
+				Anio
+				<input type="tex" name ="an">
+				<p>
+				<input type="submit" value ="Siguiente">
+				<p>
+				<p>
+				<div lass="Estilo1"><a href="http://futbolactual.webcindario.com">Pagina Principal</a></div>
+				</center>
+			</form>
+		</bady>
+</html>
